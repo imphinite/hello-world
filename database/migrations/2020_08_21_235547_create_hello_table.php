2274768dba4cc6df2a3e5f907be3bc242c3f30e4
ImPhinite <untildeathdousapar@gmail.com>
2020-08-21 17:05:01 -0700

    first commit

---
+++ database/migrations/2020_08_21_235547_create_hello_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHelloTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hello', function (Blueprint $table) {
+            $table->id();
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hello');
+    }
+}
